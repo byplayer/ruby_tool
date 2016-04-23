1d6577d61198104ef9affc6477219b5baeb4d3d3
Yukio Goto <yukio.goto@rakuten.com>
2016-04-24 01:11:40 +0800

    add fake bin

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+rm -rf bin
 rm -rf vendor/bin
 rm -rf vendor/bundler
 
 bundle install --path vendor/bundler --binstubs vendor/bin --local
+
+mkdir bin
+cp env bin/
+
+for bin_file in $( ls vendor/bin  ); do
+  cp bin_templ bin/`basename $bin_file`
+done

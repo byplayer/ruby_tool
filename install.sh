0334923cb549521e0d73c7401d4fccdcb920164b
Yukio Goto <yukio.goto@rakuten.com>
2016-04-25 09:21:33 +0800

    remove copy env to bin

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ rm -rf vendor/bundler
 bundle install --path vendor/bundler --binstubs vendor/bin --local
 
 mkdir bin
-cp env bin/
 
 for bin_file in $( ls vendor/bin  ); do
   cp bin_templ bin/`basename $bin_file`

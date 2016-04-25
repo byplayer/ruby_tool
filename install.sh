951552ae8a01423bfd56e404ae101569e6ad01a2
Yukio Goto <yukio.goto@rakuten.com>
2016-04-25 11:25:42 +0800

    set env

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+pushd `dirname ${0}` > /dev/null
+BASE_DIR=`pwd`
+popd > /dev/null
+
+export BUNDLE_GEMFILE=$BASE_DIR/Gemfile
+export RBENV_DIR=$BASE_DIR
+
 rm -rf bin
 rm -rf vendor/bin
 rm -rf vendor/bundler
